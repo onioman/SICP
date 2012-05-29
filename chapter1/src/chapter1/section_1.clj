85d67c23d1fb183e6cc7c692501e3c9f0cfe65fb
Antonio F. Lancho <onioman@gmail.com>
2012-05-29 23:05:39 +0900

    +some cleaning +exercises corrections +comments

--- chapter1/src/chapter1/section_1.clj
+++ chapter1/src/chapter1/section_1.clj
@@ -1,6 +1,7 @@
 (ns chapter1.section_1)
 
 ;exercise 1
+;==========
 (def ex1_1 10)
 (def ex1_2 (+ 5 3 4))
 (def ex1_3 (- 9 1))
@@ -28,124 +29,153 @@
      (+ ex1_a 1))) 
 
 ;exercise 2
+;==========
 (def ex2
   (/ (+ 5 4 2 -3 6 (/ 4 3))
      (* 3 (- 6 2) (- 2 7))))
 
 ;exercise 3
+;==========
 (defn ex3 [a b c]
   (let [sorted (sort (list a b c))]
     (+ (* (last sorted) (last sorted))
        (* (second sorted) (second sorted)))))
 
 ;exercise 4
+;==========
 (defn a-plus-abs-b [a b] ((if (> b 0) + -) a b))
 
 ;exercise 5
-;?????
+;==========
+(defn p [] (p))
+
+(defn order-test [x y]
+  (if (= x 0) 0 y))
+
+(comment 
+  (order-test 0 (p)))
+
+;As Sebas says, applicative-order evaluation results in a stack overflow
+;since p is infinetely recrusive. On the contrary, normal-order evaluation
+;return immediatly with the result since p is never evaluated.
 
 ;square roots
-(defn abs [x]
-  (if (> x 0) x (- 0 x)))
-
+;============
 (defn square [x]
   (* x x))
 
-(defn small-variation? [guess improved]
-  (< (abs (- guess improved)) 0.00000001))
-
-(defn good-enough? [guess improved x]
-  (or (small-variation? guess improved)
-      (< (abs (- (square guess) x)) 0.001)
-  )
-  ;(< (abs (- (square guess) x)) 0.001)
-)
+(defn good-enough? [guess x]
+  (< (Math/abs (- (square guess) x)) 0.001))
 
 (defn average [x y]
-  (/ (+ x y) 2)
-)
+  (/ (+ x y) 2))
 
 (defn improve [guess x]
-  (average guess (/ x guess))
-)
+  (average guess (/ x guess)))
 
 (defn sqrt-iter [guess x]
   (let [improved (improve guess x)]
-    (if (good-enough? guess improved x) 
+    (if (good-enough? guess x) 
       guess
-      (sqrt-iter improved x)
-    )
-  )
-)
+      (sqrt-iter improved x))))
 
 (defn sqrt [x]
-  (sqrt-iter 1.0 x)
-)
+  (sqrt-iter 1.0 x))
 
-;exercise 6V
+
+;exercise 6
+;==========
 (defn new-if [predicate then-clause else-clause]
   (cond 
     predicate   then-clause
-    :else       else-clause
-  )
-)
+    :else       else-clause))
 
-(defn new_sqrt_iter [guess x]
-  (new-if (good-enough? guess x) guess (new_sqrt_iter (improve guess x) x))
-)
+(defn new-sqrt-iter [guess x]
+  (new-if
+       (good-enough? guess x) guess 
+                              (new-sqrt-iter (improve guess x) x)))
 
-(defn new_sqrt [x]
-  (new_sqrt_iter 1.0 x)
-)
+(defn new-sqrt [x]
+  (new-sqrt-iter 1.0 x))
 
+;it gets an stack overflow becase both then and else clauses are
+;evaluate but I am not very sure why both are evaluated
+
+;exercise 7
+;==========
+(defn small-variation? [guess improved]
+  ;(let [diff (Math/abs (- guess improved))]
+  ;  (< (/ diff guess) 0.0001)))
+  (< (Math/abs (- (/ improved guess) 1)) 0.001))
+(defn sqrt-iter-variation [guess x]
+  (let [improved (improve guess x)]
+    (if (small-variation? guess improved)
+      guess
+      (sqrt-iter improved x))))
+(defn sqrt-variation [x]
+  (sqrt-iter-variation 1.0 x))
+
+; I do see any change at all using the small-variation stop condition
+
+;exercise 8
+;==========
 (defn cube-improve [guess x]
   (let [nume (+ (/ x (* guess guess)) (* 2 guess))]
-    (/ nume 3)
-  )
-)
+    (/ nume 3)))
 
-(defn cube [x]
-  (* x x x))
+(defn cube [x] (* x x x))
 
 (defn cube-good-enough? [guess improved x]
-  ;(or (small-variation? guess improved)
-  ;    (< (abs (- (square guess) x)) 0.001)
-  ;)
-  (< (abs (- (cube guess) x)) 0.001)
-)
+  (< (Math/abs (- (cube guess) x)) 0.001))
 
 (defn cube-root-iter [guess x]
   (let [improved (cube-improve guess x)]
     (if (cube-good-enough? guess improved x)
       guess
-      (cube-root-iter improved x)
-    )
-  )
-)
+      (cube-root-iter improved x))))
 
-(defn cubert [x]
-  (cube-root-iter 1.0 x)
-)
+(defn cube-root [x]
+  (cube-root-iter 1.0 x))
 
 
-; execise 1.9
+; Block structure
+; ===============
 
-; exercise 1.10
-(defn A [x y]
-  (cond
-    (= y 0) 0
-    (= x 0) (* 2 y)
-    (= y 1) 2
-    :else (A (- x 1) (A x (- y 1)))
-  )
-)
+(defn sqrt-block [x]
+  (letfn [(good-enough? [guess improved]
+            (< (Math/abs (- (/ improved guess) 1))
+               0.001))
+          (improve [guess]
+            (average guess (- x guess)))
+          (sqrt-iter [guess]
+            (let [improved (improve guess)]
+              (if (good-enough? guess improved)
+                guess
+                (sqrt-iter improved))))]
+    (sqrt-iter 1.0)))
 
-(defn f [n]
-  (A 0 n)
-)
+; Using the small-variation condition for stop here has a different
+; result.
 
-(defn g [n]
-  (A 1 n)
-)
 
-(defn h [n] (A 2 n))
+;; execise 1.9
+;
+;; exercise 1.10
+;(defn A [x y]
+;  (cond
+;    (= y 0) 0
+;    (= x 0) (* 2 y)
+;    (= y 1) 2
+;    :else (A (- x 1) (A x (- y 1)))
+;  )
+;)
+;
+;(defn f [n]
+;  (A 0 n)
+;)
+;
+;(defn g [n]
+;  (A 1 n)
+;)
+;
+;(defn h [n] (A 2 n))
