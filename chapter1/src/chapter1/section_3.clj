69b04f40a475b867f788095990a65bf56d85fbc0
Antonio F. Lancho <onioman@gmail.com>
2012-07-05 07:04:29 +0900

    exercises of section 1.3 finished.

--- chapter1/src/chapter1/section_3.clj
+++ chapter1/src/chapter1/section_3.clj
@@ -1,5 +1,6 @@
 (ns chapter1.section_3
   (:use [chapter1.section_2 :only [prime? gcd]]))
+  ;(:require clojure.contrib.generic.math-functions))
 
 ; Exercise 1.29
 ;==============
@@ -105,3 +106,214 @@
   (letfn [(ident [x] x)]
     (filtered-acum * 1 #(= 1 (gcd % n)) ident 1 inc n)))
 
+; Exercise 1.34
+;==============
+(defn f3 [g]
+  (g 2))
+
+(comment
+  (f3 #(* % %)))
+
+(comment
+  (f3 f3))
+
+; It fails since we are trying to 2 as a function:
+; (f f) = (f (f 2)) = (f (2 2)) 
+
+; Half interval method
+(defn abs [x]
+  (if (neg? x)  (- 0 x) x))
+
+(defn search [f neg-p pos-p]
+  (letfn [(close-enough? [x y]
+            (< (abs (- x y)) 0.001))]
+    (let [mid-p (/ (+ neg-p pos-p) 2)
+          test-val (f mid-p)]
+      (if (close-enough? neg-p pos-p) 
+        mid-p
+        (cond (pos? test-val) (search f neg-p mid-p)
+              (neg? test-val) (search f mid-p pos-p)
+              :else           mid-p)))))
+
+(defn half-interval-method [f a b]
+  (let [a-val (f a)
+        b-val (f b)]
+    (cond (and (neg? a-val) (pos? b-val)) (search f a b)
+          (and (neg? b-val) (pos? a-val)) (search f b a)
+          :else             (print "Values are not opposite sign"))))
+
+(comment
+  (half-interval-method sin 2.0 4.0))
+
+(comment
+  (half-interval-method #(- (* % % %) (* 2 %) 3) 1.0 2.0))
+;1.89306640625
+
+; Fixed points
+(def tolerance 0.00001)
+(defn fixed-point [f first-guess]
+  (letfn [(close-enough? [x y]
+            (< (Math/abs (- x y)) tolerance))
+          (try-it [guess]
+            (let [guess-val (f guess)]
+              (if (close-enough? guess guess-val)
+                guess-val
+                (try-it guess-val))))]
+    (try-it first-guess)))
+
+; TO TEST
+
+; Exercise 1.35
+;==============
+;TODO: Proof!
+
+(defn phi []
+  (fixed-point #(+ 1 (/ 1 %)) 1.0))
+
+;user=> (phi)                                
+;1.6180327868852458
+
+; Exercise 1.36
+;==============
+(defn fixed-point-p [f first-guess p]
+  (letfn [(close-enough? [x y]
+            (< (Math/abs (- x y)) tolerance))
+          (try-it [guess]
+            (let [guess-val (f guess)]
+              (p guess guess-val)
+              (if (close-enough? guess guess-val)
+                guess-val
+                (try-it guess-val))))]
+    (try-it first-guess)))
+
+(defn print-guess [guess next]
+  (println "guess->" guess " next->" next))
+
+(defn phi-p []
+  (fixed-point-p #(+ 1 (/ 1 %)) 1.0 print-guess))
+
+(defn x-pow-x []
+  (fixed-point-p #(/ (Math/log 1000) (Math/log %)) 2.0 print-guess))
+
+(defn average [x y]
+  (/ (+ x y) 2))
+
+(defn x-pow-x-avg []
+  (fixed-point-p #(average (/ (Math/log 1000) (Math/log %)) %) 2.0 print-guess))
+
+
+; Exercise 1.37
+;==============
+(defn cont-frac [n-fn d-fn k]
+  (letfn [(cont-frac-aux [k-i k]
+            (if (= k-i k)
+              0
+              (/ (n-fn k-i) (+ (d-fn k-i) (cont-frac-aux (inc k-i) k)))))]
+    (cont-frac-aux 0 k)))
+ 
+; TODO: iter
+;N1 / D1
+;N1 / D1 + 1 / N2 / D2
+;(defn cont-frac-iter [n-fn
+
+; Exercise 1.38
+;==============
+(defn d-euler [n]
+  (let [factor (/ (- (dec n) (mod (dec n) 3)) 3)]
+  (cond
+    (zero? n)   1
+    (= 1 n)     2
+    :else       (if (zero? (mod (dec n) 3)) (* 2 (inc factor)) 1))))
+
+(defn e-aprox [k]
+  (+ 2 (cont-frac (fn [n] 1) d-euler k)))
+
+; Exercise 1.39
+;==============
+(defn general-cont-frac [combine n-fn d-fn k]
+  (letfn [(cont-frac-aux [k-i k]
+            (if (= k-i k)
+              0
+              (/ (n-fn k-i) (combine (d-fn k-i) (cont-frac-aux (inc k-i) k)))))]
+    (cont-frac-aux 0 k)))
+
+(defn n-lambert [x k]
+  (if (zero? k) x (* x x)))
+(defn d-lambert [k]
+  (+ 1 (* 2 k)))
+
+(defn tan-approx [x k]
+  (general-cont-frac - (partial n-lambert x) d-lambert k))
+
+; Exercise 1.40
+;==============
+(def dx 0.00001)
+
+(defn deriv [g]
+  (fn [x] (/ (- (g (+ x dx)) (g x)) dx)))
+(defn newton-transform [g]
+  (fn [x] (- x (/ (g x) ((deriv g) x)))))
+(defn newtons-method [g guess]
+  (fixed-point (newton-transform g) guess))
+
+(defn cubic [a b c]
+  (fn [x]
+    (+ (* x x x)
+       (* a x x)
+       (* b x)
+       c)))
+
+; Exercise 1.41
+;==============
+(defn double-it [g]
+  (fn [x] (g (g x))))
+
+;user=> (((double-it (double-it double-it)) inc) 5)
+;21
+
+; Exercise 1.42
+;==============
+(defn compose [f g]
+  (fn [x] (f (g x))))
+
+;user=> ((compose #(* % %) inc) 6)
+;49
+
+; Exercise 1.43
+;==============
+(defn f-nth [f n]
+  (letfn ([f-nth-iter [accum i]
+           (if (= i n)  
+             accum
+             (recur (compose f accum) (inc i)))])
+    (f-nth-iter (fn [x] (f x)) 1)))
+
+; Exercise 1.44
+;==============
+(defn f-smooth [f]
+  (fn [x]
+    (/ (+ (f (- x dx))
+          (f x)
+          (f (+ x dx))) 3)))
+
+(defn f-smooth-nth [f n]
+  (letfn ([iter [accum i]
+           (if (= i n)
+             accum
+             (recur (compose f accum) (inc i)))])
+    (iter (fn [x] (f x)) 1)))
+
+; Exercise 1.45
+; =============
+; TODO
+
+; Exercise 1.46
+;==============
+(defn iterative-improve [improve good-enough?]
+  (fn [x]
+    (if (good-enough? x)
+      x
+      (recur (improve x)))))
+
+(defn sqrt-improve [x]
+  ((iterative-improve #(/ (+ % (/ x %)) 2.0) #(< (abs (- (* % %) x)) 0.001)) x))
